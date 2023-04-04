925dde284dea5aadfa9785500281852c771e3ac1
spindart <kelvin.behank@gmail.com>
2023-04-04 04:38:34 -0300

    goerli to sepolia

--- deploy.js
+++ deploy.js
@@ -5,7 +5,7 @@ const { interface, bytecode } = require('./compile');
 const provider = new HDWalletProvider(
   'REPLACE_WITH_YOUR_MNEMONIC',
   // remember to change this to your own phrase!
-  'https://rinkeby.infura.io/v3/15c1d32581894b88a92d8d9e519e476c'
+  'https://sepolia.infura.io/v3/5a364158519a47308c6dcdf9cfb0f1c1'
   // remember to change this to your own endpoint!
 );
 const web3 = new Web3(provider);
@@ -19,6 +19,7 @@ const deploy = async () => {
     .deploy({ data: bytecode })
     .send({ gas: '1000000', from: accounts[0] });
 
+  console.log(interface);
   console.log('Contract deployed to', result.options.address);
   provider.engine.stop();
 };
